9661456b79396bc8ac1b5b1b24bf0ef14bdb158e
dzucconi <mail@damonzucconi.com>
2021-03-07 17:50:41 -0500

    Supports Auspices metadata

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,52 @@
 import { take } from "ramda";
-import { configure } from "queryparams";
+import { coerce, configure } from "queryparams";
 import { FrameInterval } from "frame-interval";
 import { get, Response } from "./lib/get";
 import { NBSP, generate } from "./lib/generate";
 import { trim } from "./lib/trim";
-import { DEFAULTS } from "./lib/defaults";
+import { Defaults, DEFAULTS } from "./lib/defaults";
 
-const { params } = configure(DEFAULTS);
+const { params, query } = configure(DEFAULTS);
 
 const wrap = (x: string) => `<span>${x}</span>`;
 
 const insert = (xs: string[], separator: string) =>
   xs.join(`<i>${separator}</i>`);
 
-document.body.style.backgroundColor = params.background_color;
-document.body.style.color = params.color;
-document.body.style.fontFamily = params.font_family;
-document.body.style.fontSize = params.font_size;
-document.body.style.textAlign = params.text_align;
-
 const init = (res: Response | string) => {
   let text: string;
+  let metadata: Partial<Defaults> = {};
 
   if (typeof res === "string") {
     text = res;
+    metadata = params;
   } else {
     text = res.entity.body;
+    metadata = {
+      ...DEFAULTS,
+      ...coerce(res.metadata, DEFAULTS),
+      ...coerce(query),
+    };
   }
 
-  if (!params.break) {
+  document.body.style.backgroundColor = metadata.background_color;
+  document.body.style.color = metadata.color;
+  document.body.style.fontFamily = metadata.font_family;
+  document.body.style.fontSize = metadata.font_size;
+  document.body.style.textAlign = metadata.text_align;
+
+  if (!metadata.break) {
     text = text.replace(/ /g, NBSP);
   }
 
   const stage = document.getElementById("stage");
-  const frames = generate(text, params.width).map(wrap);
-  const separator = params.separator;
+  const frames = generate(text, metadata.width).map(wrap);
+  const separator = metadata.separator;
 
   stage.innerHTML = insert(frames, separator);
 
   const run = (limit: number) => {
-    const fi = new FrameInterval(params.fps, () => {
+    const fi = new FrameInterval(metadata.fps, () => {
       stage.innerHTML = insert(take(limit, generate.step(frames)), separator);
     });
 
